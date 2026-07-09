2aa36e9885e2157de077f3c66312c2f8c51696a8
Manasa K <k.manasa@i-exceed.com>
2026-07-09 19:56:37 +0530

    adding all the react basic files - Inception chapter

---
+++ App.js
@@ -0,0 +1,13 @@
+const heading = React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement(
+        "div",
+        { id: "child" },
+        React.createElement("h1", {}, "I'm an h1 tag")
+    )
+);
+
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
